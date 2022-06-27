8446bf12cd15a993d5017a30db99b0c04f440980
Tony LEFEVRE <tony.lefevre85@laposte.net>
2022-06-27 15:52:34 +0200

    handle error substract methode

--- substract.js
+++ substract.js
@@ -1,6 +1,8 @@
-module.exports = function substract(a, b) {
-    if (b > a) {
-      throw new Error('Negative result not allowed')
-    }
-    return a - b
+module.exports = function substrac(a, b) {
+  const res = a - b
+  if(Number.isNaN(res)) {
+    throw new Error('Invalid Operation')
   }
+ 
+  return res
+}
